e5665288c630e1dcd3d45f10f6cd9b5cb3fdc169
plamenggg <plamen_ganchev_94@abv.bg>
2024-07-09 16:57:02 +0300

    Created the src folder and index.js

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+export const HOME = 'home';
+
+export const BULGARIA = 'Bulgaria';
+
+export const SERBIA = 'Serbia';
+
+export const GREECE = 'Greece';
+
+export const CONTAINER_SELECTOR = '#container';
