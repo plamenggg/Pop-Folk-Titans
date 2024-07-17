6f90e0efc91e0ca27b83aa1f4a160582c1b77e47
plamenggg <plamen_ganchev_94@abv.bg>
2024-07-17 23:03:43 +0300

    Added toRomaniaView

---
+++ src/to-Romania-view.js
@@ -0,0 +1,30 @@
+export const toRomaniaView = () =>`
+<div id="home">
+ <h1>
+    <span style="--i: 1;">S</span>
+    <span style="--i: 2;">i</span>
+    <span style="--i: 3;">n</span>
+    <span style="--i: 4;">g</span>
+    <span style="--i: 5;">e</span>
+    <span style="--i: 6;">r</span>
+    <span style="--i: 7;">s</span>
+
+  </h1>
+  <p>Click on a picture to play a legendary song!</p>
+  <div class="home-content">
+  <div class="images">
+
+  <div class="conteiner" data-key="25">
+  <p>Laura Vass</p>
+   <img src="imgs/laura-vass.jpg" alt="laura-vass">
+</div>
+
+
+    <audio data-key="25" src="songs/Copilul de Aur  Laura Vass - Cine e inima mea   غنية رومانية جميلة (INTERNATIONAL HIT  DiBi TV).mp3"></audio>
+  
+  
+
+    </div>
+  </div>
+</div>
+`;
